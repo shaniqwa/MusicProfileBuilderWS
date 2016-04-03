e2896b1e7ced0998d1323f6c29fe110636fb0f12
Shani <s.almog88@gmail.com>
2016-04-03 15:52:23 +0300

    add category to genre schema

--- genres_schema.js
+++ genres_schema.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose'),
 	schema = mongoose.Schema;
 
 var genresSchema = new schema({
+	category: String,
 	name: String,
 	related_to:  [{ type: String}]
 }, {collection: 'genres'});
